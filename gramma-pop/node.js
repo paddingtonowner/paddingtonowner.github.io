e6c8a21356adbb60047d3cba701b10d78b4a9cd0
paddingtonowner <dianapatrice@gmail.com>
2024-12-26 14:02:13 -0500

    Update node.js

--- gramma-pop/node.js
+++ gramma-pop/node.js
@@ -4,7 +4,7 @@ var timeEl = document.getElementById("time");
 function loop() {
   // Your code here
   time = new Date().getTime();
-  timeEl.textcontent = time;
+  timeEl.textContent = time;
 
   // Call the loop function again after 33.33 ms (1000 ms / 30 fps = 33.33 ms)
   setTimeout(loop, 1000 / 30);
