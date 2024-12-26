233307cadc699ffaf0dc5a81284ce1047e4801f3
paddingtonowner <dianapatrice@gmail.com>
2024-12-26 13:18:15 -0500

    Update node.js

--- gramma-pop/node.js
+++ gramma-pop/node.js
@@ -1,1 +1,14 @@
+var time;
+var timeEl = document.getElementById("time");
 
+function loop() {
+  // Your code here
+  time = new Date().getTime();
+  timeEl.textcontent = time;
+
+  // Call the loop function again after 33.33 ms (1000 ms / 30 fps = 33.33 ms)
+  setTimeout(loop, 1000 / 30);
+}
+
+// Start the loop
+loop();
