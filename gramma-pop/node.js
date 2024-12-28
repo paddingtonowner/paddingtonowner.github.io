8a126c92fd9eb95a4c846070af53eb4881641e53
paddingtonowner <dianapatrice@gmail.com>
2024-12-28 11:50:01 -0500

    Update node.js

--- gramma-pop/node.js
+++ gramma-pop/node.js
@@ -5,7 +5,7 @@ function updateClock() {
     const seconds = now.getSeconds().toString().padStart(2, '0');
 
     const currentTime = `${hours}:${minutes}:${seconds}`;
-    if(document.getElementById("button").textContent === "Ta-Da!") {
+    if(document.getElementById("button").textContent === "Click to hide the time.") {
         document.getElementById('clock').textContent = currentTime;
     } else {
         document.getElementById('clock').textContent = '';
@@ -13,13 +13,17 @@ function updateClock() {
 }
 
 function changeButton() {
-    const b = document.getElementById("button").textContent = "Ta-Da!";
+    const b = document.getElementById("button")
+    if(b.textContent === "Click to see the time.") {
+        b.textContent = "Click to hide the time.";
+    } else {
+        b.textContent = "Click to see the time.";
+    }
 };
 
 document.getElementById("button").addEventListener("click",changeButton);
 
 // Update the clock every second
-setInterval(updateClock, 1000);
+setInterval(updateClock, 0);
 
-// Initialize the clock immediately
 updateClock();
