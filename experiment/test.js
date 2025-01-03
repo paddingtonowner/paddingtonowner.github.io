11f56c68818bf7c53aa79067cbc3adc3aad6ef58
paddingtonowner <dianapatrice@gmail.com>
2025-01-03 15:41:33 -0500

    Delete experiment directory

--- experiment/test.js
+++
@@ -1,9 +0,0 @@
-void setup() {
-            size(400, 400);
-            background(255);
-        }
-
-        void draw() {
-            fill(0);
-            ellipse(mouseX, mouseY, 50, 50);
-        }
