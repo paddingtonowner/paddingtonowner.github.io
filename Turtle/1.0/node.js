1f4fe8f31e692981f6550302a1f6db9df4febf3c
paddingtonowner <dianapatrice@gmail.com>
2025-01-22 12:07:48 -0500

    Create node.js

---
+++ Turtle/1.0/node.js
@@ -0,0 +1,18 @@
+function downloadFiles() {
+    const file1Url = './file1.py';
+
+    const file1Name = 'file1.py';
+
+    downloadFile(file1Url, file1Name);
+}
+
+function downloadFile(url, fileName) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+downloadFiles();
